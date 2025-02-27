06606e678b3b745e41b969a8e8905cb7c7e6b866
TanmayDev2311 <tanmaydev2311@gmail.com>
2025-02-27 22:42:31 +0530

    modified UI

--- script.js
+++ script.js
@@ -1,20 +1,49 @@
 //* AUTHOR : Tanmay Dev Prasad
 //* Date : 23 Feb 2025
 
+//accepting user's choice from the menu
+var userChoice;
+
+document.querySelector(".choiceBtns").addEventListener("click", (event) => {
+    //if user chooses x
+    if (event.target.classList.contains("x")) {
+        userChoice = "×"
+
+        document.querySelector(".choiceMenu").remove(); //removing the choice menu after user selects their choice
+        document.getElementById("header").style.opacity = "1"; //retaining the opacity
+        document.getElementById("main").style.opacity = "1";
+    }
+    //if user chooses O
+    else if (event.target.classList.contains("O")) {
+        userChoice = "◯"
+
+        document.querySelector(".choiceMenu").remove(); //removing the choice menu after user selects their choice
+        document.getElementById("header").style.opacity = "1"; //retaining the opacity
+        document.getElementById("main").style.opacity = "1";
+    }
+}
+)
+
 //event delegation on all the boxes
 const playground = document.querySelector(".arena")
-playground.addEventListener("click",(event) => {
+playground.addEventListener("click", (event) => {
 
     const cross = document.createElement("span")
     cross.innerHTML = "×"
-    
+
     const circle = document.createElement("p")
     circle.innerHTML = "◯"
-  
+
     if (event.target.classList.contains("box")) {
         box = event.target;
-        // box.append(cross)
-        box.append(circle)
+
+        if(userChoice == "×"){
+            box.append(cross)
+        }
+        else{
+            box.append(circle)
+        }
+
     }
 }
 ) 
